26d270e41137599df74918eb2a92c532e0e5b303
hurricanenara <naralee0124@gmail.com>
2023-09-12 02:24:47 +0900

    add live url output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,8 +11,8 @@ function run() {
   // 2. upload files
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-
-  core.notice("hello from my custom JS action");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
